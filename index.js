8bd64a1d30907b38ddc964ff34bddda3b452ab91
Nilton <niltonroese@gmail.com>
2022-03-03 08:27:23 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) +1;
+
+const num3 = 24;
+const num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,7,9,11,14,15,18,20);
